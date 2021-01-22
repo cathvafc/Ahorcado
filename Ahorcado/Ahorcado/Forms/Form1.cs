1ed349a4a4499ed2576266e7d0b3e51880adca92
cathvafc <miguelangelgomezlope@gmail.com>
2021-01-22 10:09:40 +0100

    Esquema interficie gráfica

---
+++ Ahorcado/Ahorcado/Forms/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ahorcado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Iniciar juego
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //El número de pistas restantes es:
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Pedir Pista
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //A
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            //B
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            //C
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            //D
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            //E
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //F
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //G
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //H
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //I
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //J
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            //K
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            //L
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            //M
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //N
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //Ñ
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            //O
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            //P
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            //Q
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            //R
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            //S
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            //T
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //U
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //V
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //W
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //X
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            //Y
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            //Z
+        }
+    }
+}
